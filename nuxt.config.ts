0f867e6b236e4fd48ac132cab08b42b3fd3416bb
ChoHyeonSeok <tksk2884@naver.com>
2024-10-09 11:12:47 +0900

    Config 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@element-plus/nuxt"],
+  compatibilityDate: "2024-10-09",
+});
