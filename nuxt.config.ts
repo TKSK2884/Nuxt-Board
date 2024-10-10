42a7aa99cc23a0e61e66e6740aeea356afcef25e
Rufree <tksk2884@gmail.com>
2024-10-10 18:25:23 +0900

    main CSS 추가, nuxt config 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@element-plus/nuxt"],
-  compatibilityDate: "2024-10-09",
+    devtools: { enabled: true },
+    modules: ["@element-plus/nuxt"],
+    compatibilityDate: "2024-10-09",
+    css: ["~/assets/css/main.css"],
 });
