6be3617be8136067f5d8ced8ff7061e3151d5430
Rufree <tksk2884@gmail.com>
2024-10-23 15:26:53 +0900

    tiptap 들여쓰기 확장 추가

--- pages/index.vue
+++ pages/index.vue
@@ -6,32 +6,99 @@
                 v-for="(board, index) in boardList"
                 :key="'boardList-' + index"
                 :class="$style.board"
+                :board="board"
             />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-const boardList: {}[] = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
+import type { Board, Post } from "~/structure/type";
+
+const config = useRuntimeConfig();
+
+const boardList: Board[] = [
+    {
+        id: 1,
+        title: "반려동물",
+        category: "pet",
+        post: [
+            {
+                id: 1,
+                postTitle: "우리 집에 새 가족이 생김",
+                feedback: 5,
+                time: 1214124,
+            },
+            {
+                id: 1,
+                postTitle: "우리 집에 새 가족이 생김",
+                feedback: 5,
+                time: 1214124,
+            },
+            {
+                id: 1,
+                postTitle: "우리 집에 새 가족이 생김",
+                feedback: 5,
+                time: 1214124,
+            },
+            {
+                id: 1,
+                postTitle: "우리 집에 새 가족이 생김",
+                feedback: 5,
+                time: 1214124,
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "게임",
+        category: "game",
+        post: [
+            {
+                id: 1,
+                postTitle: "우리 집에 새 가족이 생김",
+                feedback: 5,
+                time: 1214124,
+            },
+        ],
+    },
 ];
+
+onMounted(() => {
+    if (process.server) {
+        return;
+    }
+
+    let sessionAccessToken: string =
+        sessionStorage.getItem("accessToken") ?? "";
+
+    const accessToken: string = localStorage.getItem("accessToken") ?? "";
+
+    if (accessToken != "") {
+        localStorage.removeItem("accessToken");
+        sessionStorage.setItem("accessToken", accessToken);
+
+        sessionAccessToken = accessToken;
+    }
+
+    if (sessionAccessToken != "") {
+        getUserNickname(sessionAccessToken);
+    }
+});
+
+const getUserNickname = async (token: string) => {
+    if (token == "") {
+        return;
+    }
+
+    const result: any = await $fetch("/member/info", {
+        baseURL: config.public.apiBase,
+        method: "GET",
+        headers: {
+            authorization: token,
+        },
+    });
+};
 
 const getBoardList = () => {};
 </script>
