34730ac1ed7b68a6b349f44ee052c084a70a7c3c
Rufree <tksk2884@gmail.com>
2024-10-26 15:58:04 +0900

    로그인 관련 로직 구현

---
+++ stores/auth.ts
@@ -0,0 +1,65 @@
+import { defineStore } from "pinia";
+
+interface UserState {
+    nickname: string;
+    isLoggedIn: boolean;
+}
+
+export const useAuthStore = defineStore("user", () => {
+    const config = useRuntimeConfig();
+    const accessToken = useCookie("accessToken");
+
+    const userState: Ref<UserState | null> = ref(null);
+
+    const checkAuth = async () => {
+        if (accessToken.value == null) {
+            return;
+        }
+
+        try {
+            const result: any = await $fetch("/auth/status", {
+                baseURL: config.public.apiBase,
+                method: "GET",
+                query: {
+                    accessToken: accessToken.value,
+                },
+            });
+
+            if (!result.success) {
+                userState.value = null;
+
+                return;
+            }
+
+            userState.value = {
+                nickname: result.nickname,
+                isLoggedIn: true,
+            };
+        } catch (error) {
+            console.error("인증 상태 확인 오류", error);
+            userState.value = null;
+        }
+    };
+
+    // const setToken = (token: string) => {
+    //     accessToken.value = token;
+    //     console.log("work");
+    // };
+
+    const login = (userNickname: string, token: string) => {
+        userState.value = { nickname: userNickname, isLoggedIn: true };
+        accessToken.value = token;
+    };
+
+    const logout = () => {
+        userState.value = null;
+        accessToken.value = null;
+    };
+
+    return {
+        userState,
+        login,
+        logout,
+        checkAuth,
+    };
+});
