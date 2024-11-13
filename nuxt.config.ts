60a71ced8cb00b94ee065beb211da080481d5f5a
Rufree <tksk2884@gmail.com>
2024-11-13 23:41:42 +0900

    ckeditor 플러그인 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
             apiBase: process.env.VUE_APP_API_URL || "http://localhost:8455",
         },
     },
+
+    plugins: [{ src: "~/plugins/ckeditor.client.ts", mode: "client" }],
+
     modules: ["@element-plus/nuxt", "@pinia/nuxt"],
     compatibilityDate: "2024-10-09",
     css: ["~/assets/css/main.css"],
