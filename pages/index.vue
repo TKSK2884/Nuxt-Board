5d128cba90f533f02ecbc91dd8a843b8794ba3e9
Rufree <tksk2884@gmail.com>
2024-11-26 18:02:16 +0900

    게시판별 버튼 추가, 기존 스타일링 변경

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
     <div :class="$style.index">
-        <div :class="$style.banner"></div>
+        <div :class="$style.buttons">
+            <el-button
+                v-for="(item, index) in boardList"
+                :key="'boardButton-' + index"
+                @click="goBoard(item.slug)"
+                :class="$style.button"
+                round
+            >
+                {{ item.title }}
+            </el-button>
+        </div>
+
         <div :class="$style.inner">
             <BoardItem
                 v-for="(board, index) in boardList"
@@ -20,10 +31,6 @@ const config = useRuntimeConfig();
 const boardList: Ref<BoardList[]> = ref([]);
 
 onMounted(() => {
-    if (process.server) {
-        return;
-    }
-
     getBoardList();
 });
 
@@ -35,6 +42,15 @@ const getBoardList = async () => {
 
     boardList.value = result.data;
 };
+
+const goBoard = (category: string) => {
+    navigateTo({
+        path: "/board",
+        query: {
+            category: category,
+        },
+    });
+};
 </script>
 
 <style lang="scss" module>
@@ -44,12 +60,32 @@ const getBoardList = async () => {
     width: 100%;
     height: 100%;
 
+    > .buttons {
+        max-width: 1080px;
+        padding: 8px;
+
+        display: flex;
+        gap: 8px;
+        margin-inline: auto;
+        overflow-x: auto;
+
+        > .button {
+            &:hover,
+            &:active {
+                background-color: #f3f4f6;
+                color: #606266;
+                border-color: #f3f4f6;
+            }
+        }
+    }
+
     > .inner {
         max-width: 1080px;
 
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
+        gap: 32px;
 
         padding: 8px;
 
@@ -58,62 +94,64 @@ const getBoardList = async () => {
         }
 
         > .board {
-            padding: 8px;
             margin-block: 8px;
 
             flex-shrink: 0;
             flex-grow: 0;
-            flex-basis: 50%;
+            flex-basis: calc(50% - 16px);
+
+            border: 1px solid #e5e7eb;
+            border-radius: 8px;
 
             @include mobile {
                 width: 100%;
             }
         }
 
-        > .board::after {
-            width: 100%;
-            height: 100%;
+        // > .board::after {
+        //     width: 100%;
+        //     height: 100%;
 
-            content: "";
+        //     content: "";
 
-            display: block;
+        //     display: block;
 
-            position: relative;
-            bottom: -16px;
+        //     position: relative;
+        //     bottom: -16px;
 
-            border-bottom: 1px solid #bbb;
+        //     border-bottom: 1px solid #bbb;
 
-            @include mobile {
-                bottom: -8px;
-            }
-        }
+        //     @include mobile {
+        //         bottom: -8px;
+        //     }
+        // }
 
-        > .board:nth-child(odd) {
-            border-right: 1px solid #bbb;
+        // > .board:nth-child(odd) {
+        //     border-right: 1px solid #bbb;
 
-            @include mobile {
-                border-right: none;
-            }
-        }
+        //     @include mobile {
+        //         border-right: none;
+        //     }
+        // }
 
-        > .board:nth-last-child(1)::after,
-        > .board:nth-last-child(2)::after {
-            display: none;
-        }
+        // > .board:nth-last-child(1)::after,
+        // > .board:nth-last-child(2)::after {
+        //     display: none;
+        // }
 
-        > .board:nth-last-child(2)::after {
-            @include mobile {
-                display: block;
-            }
-        }
+        // > .board:nth-last-child(2)::after {
+        //     @include mobile {
+        //         display: block;
+        //     }
+        // }
 
-        > .board:last-child(odd) {
-            margin-right: auto;
+        // > .board:last-child(odd) {
+        //     margin-right: auto;
 
-            @include mobile {
-                margin-right: initial;
-            }
-        }
+        //     @include mobile {
+        //         margin-right: initial;
+        //     }
+        // }
     }
 }
 </style>
